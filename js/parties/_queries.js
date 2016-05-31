ac04fdd912ff4005278c88229e0b3254ad2b953f
Lemon <lemon@hirelemon.com>
2016-05-31 16:14:16 -0500

    Just getting this ready for publishing.

---
+++ js/parties/_queries.js
@@ -0,0 +1,16 @@
+var rounds = [
+  {
+    query: "color of women's ",
+    right: "color of women's discharge",
+    wrong1: "color of women's suffrage movement",
+    wrong2: "color of women's day",
+    wrong3: "color of women's nipples"
+  },
+  {
+    query: "why do ",
+    right: "why do we yawn",
+    wrong1: "why do men cheat",
+    wrong2: "why do men have nipples",
+    wrong3: "why do people snore"
+  },
+];
