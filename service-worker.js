9e827106d9f7ee0901172c56a872b2516c5057c1
Lemon <AhoyLemon@users.noreply.github.com>
2018-03-22 14:48:42 -0500

    offlineURL variable.

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,7 @@
 
 const PRECACHE = 'precache-v24';
 const RUNTIME = 'runtime';
+const offlineURL = '/offline.html';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
