646fc13dd310ba5ef9ff8acfe549f647f81672de
Lemon <AhoyLemon@users.noreply.github.com>
2018-03-22 13:04:57 -0500

    precache-v107

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,26 @@
 'use strict';
 
-const cacheName = 'v1.06';
-const offlineUrl = '/offline.html';
+const PRECACHE = 'precache-v107';
+const RUNTIME = 'runtime';
 
-self.addEventListener('install', e => {
-  // once the SW is installed, go ahead and fetch the resources
-  // to make this work offline
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/manifest.json',
-        '/css/idiots.css',
-        '/js/min/idiots.min.js',
-        '/js/libraries/jquery.min.js',
-        '/svg/offline.svg',
-        offlineUrl
-      ]).then(() => self.skipWaiting());
-    })
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  './', // Alias for index.html
+  '/index.html',
+  '/manifest.json',
+  '/css/idiots.css',
+  '/js/min/idiots.min.js',
+  '/js/libraries/jquery.min.js',
+  '/svg/offline.svg',
+  offlineUrl
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
 
